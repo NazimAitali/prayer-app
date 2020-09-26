e343db45f2e6fb73221347d85fb04bfbc152ea88
nazim <nazimaitali88@gmail.com>
2020-09-26 11:54:22 +0100

    Updates

---
+++ precache-manifest.df937560d0e0717afcfb409757164130.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3adc219c4d92342d99bc5320d40a41f6",
+    "url": "/prayer-app/index.html"
+  },
+  {
+    "revision": "236fb912ca4632ea5a73",
+    "url": "/prayer-app/static/css/2.cd846464.chunk.css"
+  },
+  {
+    "revision": "d7f84425a69906ee924b",
+    "url": "/prayer-app/static/css/main.a3c7fc4f.chunk.css"
+  },
+  {
+    "revision": "236fb912ca4632ea5a73",
+    "url": "/prayer-app/static/js/2.f2ddd211.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/prayer-app/static/js/2.f2ddd211.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7f84425a69906ee924b",
+    "url": "/prayer-app/static/js/main.7be7ebaa.chunk.js"
+  },
+  {
+    "revision": "edfe2e6e733575b02418",
+    "url": "/prayer-app/static/js/runtime-main.783cef46.js"
+  },
+  {
+    "revision": "6a2d8bd63654861148b8c8b681acf20a",
+    "url": "/prayer-app/static/media/2.6a2d8bd6.png"
+  },
+  {
+    "revision": "a8ae090591464088288a31b3d08c3695",
+    "url": "/prayer-app/static/media/bg-results.a8ae0905.png"
+  },
+  {
+    "revision": "6a2586c4aa15459aca93ce098b6b4dd3",
+    "url": "/prayer-app/static/media/bg.6a2586c4.jpg"
+  },
+  {
+    "revision": "949d0574376f2a6c0ddac4b856b535b4",
+    "url": "/prayer-app/static/media/error.949d0574.png"
+  },
+  {
+    "revision": "f908c3e44792f9031efed3f2c5203862",
+    "url": "/prayer-app/static/media/no-data.f908c3e4.png"
+  }
+]);
